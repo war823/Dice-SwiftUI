2ce5acdd43a3e90fc1a39a51077bdd25432f64ba
war823 <maximkovol@gmail.com>
2023-08-18 21:48:29 +0200

    Initial Commit

---
+++ Dice-SwiftUI/Dice_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_SwiftUIApp.swift
+//  Dice-SwiftUI
+//
+//  Created by Max Volokhovskyi on 18/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Dice_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
